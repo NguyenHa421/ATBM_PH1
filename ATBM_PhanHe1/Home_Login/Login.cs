5bf3af9c065b6bed91002f7e3f132a5835d46693
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-03-27 17:16:33 +0700

    Create Login.cs

---
+++ ATBM_PhanHe1/Home_Login/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.Home_Login
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
