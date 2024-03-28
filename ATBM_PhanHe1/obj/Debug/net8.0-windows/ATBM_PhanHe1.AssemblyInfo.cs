a4f8685fcefe9ac7286dc05863c8d0545a73f9f0
SvvPhenex <phucdat112003@gmail.com>
2024-03-28 10:08:46 +0700

    cài xong ODAC

--- ATBM_PhanHe1/obj/Debug/net8.0-windows/ATBM_PhanHe1.AssemblyInfo.cs
+++ ATBM_PhanHe1/obj/Debug/net8.0-windows/ATBM_PhanHe1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATBM_PhanHe1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00979fa7957ccc89f7e46ae6e82beabaa3e03e56")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATBM_PhanHe1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATBM_PhanHe1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
