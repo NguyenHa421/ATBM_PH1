4ea30df007ddc6b43576c6d91532f24faaac9b50
SvvPhenex <phucdat112003@gmail.com>
2024-03-28 14:14:14 +0700

    ...

---
+++ ATBM_PhanHe1/DAO/DataProvider.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ATBM_PhanHe1.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+
+        private string  connectionStr = "User Id=sys;Password=Phucdat11;Data Source=localhost:1521/QLSinhVien_DoAn;";
+        public static DataProvider Instance
+        {
+            get { if (instance == null) instance = new DataProvider(); return DataProvider.instance; }
+            private set { DataProvider.instance = value; }
+
+        }
+
+        private DataProvider() { }
+
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+
+            using (OracleConnection connection = new OracleConnection(connectionStr))
+            {
+
+                connection.Open();
+                OracleCommand command = new OracleCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains("@"))
+                        {
+                            command.Parameters.Add(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                OracleDataAdapter adapter = new OracleDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+
+            using (OracleConnection connection = new OracleConnection(connectionStr))
+            {
+
+                connection.Open();
+                OracleCommand command = new OracleCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains("@"))
+                        {
+                            command.Parameters.Add(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return data;
+        }
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+
+            using (OracleConnection connection = new OracleConnection(connectionStr))
+            {
+
+                connection.Open();
+                OracleCommand command = new OracleCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains("@"))
+                        {
+                            command.Parameters.Add(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteScalar();
+
+                connection.Close();
+            }
+            return data;
+        }
+    }
+
+}
---
+++ ATBM_PhanHe1/Interface/Homepage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.Interface
+{
+    public partial class Homepage : Form
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void Homepage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Homepage_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
