106ba099529b6578eb4af2d79eeafca7be4e9aa1
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-03-29 01:14:28 +0700

    Update Login.Designer.cs

--- ATBM_PhanHe1/Home_Login/Login.Designer.cs
+++ ATBM_PhanHe1/Home_Login/Login.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Image = Properties.Resources.Home_Pic1;
+            pictureBox1.Location = new Point(77, 147);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(413, 297);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(42, 107, 167);
+            label1.Location = new Point(386, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(230, 54);
+            label1.TabIndex = 1;
+            label1.Text = "Đăng nhập";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(549, 174);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 31);
+            label2.TabIndex = 2;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(549, 306);
+            label3.Name = "label3";
+            label3.Size = new Size(110, 31);
+            label3.TabIndex = 3;
+            label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(549, 225);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "   Username";
+            textBox1.Size = new Size(312, 38);
+            textBox1.TabIndex = 4;
+            textBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(549, 361);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "   Password";
+            textBox2.Size = new Size(312, 38);
+            textBox2.TabIndex = 5;
+            textBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.BackgroundImage = Properties.Resources.b_r;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(699, 453);
+            button1.Name = "button1";
+            button1.Size = new Size(162, 64);
+            button1.TabIndex = 6;
+            button1.TabStop = false;
+            button1.Text = "Đăng nhập";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(42, 107, 167);
+            BackgroundImage = Properties.Resources.Background1;
+            ClientSize = new Size(957, 590);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
