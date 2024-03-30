5f6127712cc41d48808ea9d79f77df913647930c
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-03-30 23:53:32 +0700

    Create Delete_U.cs

---
+++ ATBM_PhanHe1/User/Delete_U.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.User
+{
+    public partial class Delete_U : Form
+    {
+        public Delete_U()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
