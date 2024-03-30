db73cbf776b635894b0348e04b69327b6f68b37f
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-03-30 23:56:16 +0700

    Create Revorke_R.cs

---
+++ ATBM_PhanHe1/Users_Roles/Revorke_R.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.Users_Roles
+{
+    public partial class Revorke_R : Form
+    {
+        public Revorke_R()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
