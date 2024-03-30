bd94c517701299d0d6edda2eaa8cdb9fef5738d2
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-03-30 23:56:12 +0700

    Create Grant_U_R.Designer.cs

---
+++ ATBM_PhanHe1/Users_Roles/Grant_U_R.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace ATBM_PhanHe1.Users_Roles
+{
+    partial class Grant_U_R
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            bt_View = new Button();
+            bt_revoke = new Button();
+            btn_Back = new Button();
+            dataGridView1 = new DataGridView();
+            tb_R = new TextBox();
+            lb_n_R = new Label();
+            tb_user = new TextBox();
+            lb_Name = new Label();
+            btn_grant = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // bt_View
+            // 
+            bt_View.BackColor = Color.White;
+            bt_View.BackgroundImage = Properties.Resources.button_round2;
+            bt_View.FlatAppearance.BorderSize = 0;
+            bt_View.FlatStyle = FlatStyle.Flat;
+            bt_View.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            bt_View.ForeColor = SystemColors.Window;
+            bt_View.Location = new Point(613, 243);
+            bt_View.Name = "bt_View";
+            bt_View.Size = new Size(91, 38);
+            bt_View.TabIndex = 51;
+            bt_View.Text = "Xem";
+            bt_View.UseVisualStyleBackColor = false;
+            // 
+            // bt_revoke
+            // 
+            bt_revoke.BackColor = Color.White;
+            bt_revoke.BackgroundImage = Properties.Resources.button_round2;
+            bt_revoke.FlatAppearance.BorderSize = 0;
+            bt_revoke.FlatStyle = FlatStyle.Flat;
+            bt_revoke.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            bt_revoke.ForeColor = SystemColors.Window;
+            bt_revoke.Location = new Point(467, 328);
+            bt_revoke.Name = "bt_revoke";
+            bt_revoke.Size = new Size(91, 38);
+            bt_revoke.TabIndex = 49;
+            bt_revoke.Text = "Thu hồi";
+            bt_revoke.UseVisualStyleBackColor = false;
+            // 
+            // btn_Back
+            // 
+            btn_Back.BackColor = Color.White;
+            btn_Back.BackgroundImage = Properties.Resources.button_round2;
+            btn_Back.FlatAppearance.BorderSize = 0;
+            btn_Back.FlatStyle = FlatStyle.Flat;
+            btn_Back.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_Back.ForeColor = SystemColors.Window;
+            btn_Back.Location = new Point(311, 328);
+            btn_Back.Name = "btn_Back";
+            btn_Back.Size = new Size(91, 38);
+            btn_Back.TabIndex = 48;
+            btn_Back.Text = "Quay lại";
+            btn_Back.UseVisualStyleBackColor = false;
+            btn_Back.Click += btn_Back_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.WhiteSmoke;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(34, 107);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(271, 196);
+            dataGridView1.TabIndex = 47;
+            // 
+            // tb_R
+            // 
+            tb_R.BorderStyle = BorderStyle.FixedSingle;
+            tb_R.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_R.Location = new Point(439, 177);
+            tb_R.Name = "tb_R";
+            tb_R.Size = new Size(265, 34);
+            tb_R.TabIndex = 44;
+            // 
+            // lb_n_R
+            // 
+            lb_n_R.AutoSize = true;
+            lb_n_R.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lb_n_R.Location = new Point(311, 179);
+            lb_n_R.Name = "lb_n_R";
+            lb_n_R.Size = new Size(85, 28);
+            lb_n_R.TabIndex = 43;
+            lb_n_R.Text = "Tên role";
+            // 
+            // tb_user
+            // 
+            tb_user.BorderStyle = BorderStyle.FixedSingle;
+            tb_user.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_user.Location = new Point(439, 105);
+            tb_user.Name = "tb_user";
+            tb_user.Size = new Size(265, 34);
+            tb_user.TabIndex = 42;
+            // 
+            // lb_Name
+            // 
+            lb_Name.AutoSize = true;
+            lb_Name.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lb_Name.Location = new Point(311, 107);
+            lb_Name.Name = "lb_Name";
+            lb_Name.Size = new Size(89, 28);
+            lb_Name.TabIndex = 41;
+            lb_Name.Text = "Tên user";
+            // 
+            // btn_grant
+            // 
+            btn_grant.BackColor = Color.White;
+            btn_grant.BackgroundImage = Properties.Resources.button_round2;
+            btn_grant.FlatAppearance.BorderSize = 0;
+            btn_grant.FlatStyle = FlatStyle.Flat;
+            btn_grant.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_grant.ForeColor = SystemColors.Window;
+            btn_grant.Location = new Point(613, 328);
+            btn_grant.Name = "btn_grant";
+            btn_grant.Size = new Size(91, 38);
+            btn_grant.TabIndex = 52;
+            btn_grant.Text = "Cấp role";
+            btn_grant.UseVisualStyleBackColor = false;
+            // 
+            // Grant_U_R
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(739, 484);
+            Controls.Add(btn_grant);
+            Controls.Add(bt_View);
+            Controls.Add(bt_revoke);
+            Controls.Add(btn_Back);
+            Controls.Add(dataGridView1);
+            Controls.Add(tb_R);
+            Controls.Add(lb_n_R);
+            Controls.Add(tb_user);
+            Controls.Add(lb_Name);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Grant_U_R";
+            Text = "Grant_U_R";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button bt_View;
+        private Button bt_revoke;
+        private Button btn_Back;
+        private DataGridView dataGridView1;
+        private TextBox tb_R;
+        private Label lb_n_R;
+        private TextBox tb_user;
+        private Label lb_Name;
+        private Button btn_grant;
+    }
+}
