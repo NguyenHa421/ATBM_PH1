d69d4017c186d0b3390be22511edae87435832ae
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-03-30 23:55:59 +0700

    Create Grant_U.cs

---
+++ ATBM_PhanHe1/Users_Roles/Grant_U.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.Users_Roles
+{
+    public partial class Grant_U : Form
+    {
+        public Grant_U()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
