f6e611d3e77cd3c971fe0bcb92a84a58515babbe
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-03-30 23:52:35 +0700

    Update User_Role.cs

--- ATBM_PhanHe1/Interface/User_Role.cs
+++ ATBM_PhanHe1/Interface/User_Role.cs
@@ -16,5 +16,79 @@ namespace ATBM_PhanHe1.Interface
         {
             InitializeComponent();
         }
+        private Form currentFormChild;
+        private void OpenChildForm(Form childForm)
+        {
+            if (currentFormChild != null)
+            {
+                currentFormChild.Close();
+            }
+            currentFormChild = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            Parent_panel.Controls.Add(childForm);
+            Parent_panel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+        private void CloseAllFormsExceptFirst()
+        {
+            Form[] forms = Application.OpenForms.Cast<Form>().ToArray();
+            for (int i = 1; i < forms.Length; i++)
+            {
+                forms[i].Close();
+            }
+        }
+        private void DSTK_button_Click(object sender, EventArgs e)
+        {
+            Interface.Homepage homepage = new Interface.Homepage();
+            this.Hide();
+            homepage.ShowDialog();
+            this.Show();
+        }
+
+        private void QLQ_button_Click(object sender, EventArgs e)
+        {
+            Interface.Permission permission = new Interface.Permission();
+            this.Hide();
+            permission.ShowDialog();
+            this.Show();
+        }
+
+        private void pic_logout_Click(object sender, EventArgs e)
+        {
+            CloseAllFormsExceptFirst();
+        }
+
+        private void btn_create_user_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new User.Create_U());
+        }
+
+        private void btn_delete_user_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new User.Delete_U());
+        }
+
+        private void btn_create_role_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Role.Create_R());
+        }
+
+        private void btn_delete_role_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Role.Delete_R());
+        }
+
+        private void btn_update_user_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new User.Update_U());
+        }
+
+        private void btn_update_role_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Role.Update_R());
+        }
     }
 }
