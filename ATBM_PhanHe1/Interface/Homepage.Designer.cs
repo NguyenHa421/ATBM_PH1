e51048cd3ec60c73931815668710172d4cd14069
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-03-31 20:30:52 +0700

    Update Homepage.Designer.cs

--- ATBM_PhanHe1/Interface/Homepage.Designer.cs
+++ ATBM_PhanHe1/Interface/Homepage.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             pn_main = new Panel();
+            pic_refresh_U = new PictureBox();
+            dtGrid_user = new DataGridView();
+            panel5 = new Panel();
+            tb_search_user = new TextBox();
+            pic_search_user = new PictureBox();
+            btn_search_user = new Button();
+            lab_dstk = new Label();
             dtGrid_main = new DataGridView();
             tb_search = new TextBox();
             pic_search = new PictureBox();
@@ -44,72 +51,109 @@
             pn_qlq = new Panel();
             btn_qlq = new Button();
             pn_main.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pic_refresh_U).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dtGrid_user).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pic_search_user).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dtGrid_main).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic_search).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pic_khtn).BeginInit();
             pn_dstk.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pic_logout).BeginInit();
             pn_qlur.SuspendLayout();
             pn_qlq.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dtGrid_main).BeginInit();
-            panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pic_search).BeginInit();
             SuspendLayout();
             // 
             // pn_main
             // 
             pn_main.BackColor = SystemColors.Window;
-            pn_main.Controls.Add(pn_main);
+            pn_main.Controls.Add(pic_refresh_U);
+            pn_main.Controls.Add(dtGrid_user);
+            pn_main.Controls.Add(panel5);
+            pn_main.Controls.Add(btn_search_user);
+            pn_main.Controls.Add(lab_dstk);
             pn_main.Location = new Point(220, 54);
             pn_main.Name = "pn_main";
             pn_main.Size = new Size(739, 536);
             pn_main.TabIndex = 0;
             // 
-            // pn_dstk
+            // pic_refresh_U
             // 
-            pn_dstk.BackColor = Color.FromArgb(177, 213, 246);
-            pn_dstk.Controls.Add(btn_dstk);
-            pn_dstk.Controls.Add(label2);
-            pn_dstk.Location = new Point(0, 254);
-            pn_dstk.Name = "pn_dstk";
-            pn_dstk.Size = new Size(220, 76);
-            pn_dstk.TabIndex = 2;
+            pic_refresh_U.Image = Properties.Resources.refresh_buttons;
+            pic_refresh_U.Location = new Point(561, 76);
+            pic_refresh_U.Name = "pic_refresh_U";
+            pic_refresh_U.Size = new Size(49, 39);
+            pic_refresh_U.SizeMode = PictureBoxSizeMode.Zoom;
+            pic_refresh_U.TabIndex = 24;
+            pic_refresh_U.TabStop = false;
             // 
-            // label2
+            // dtGrid_user
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(7, 21);
-            label2.Name = "label2";
-            label2.Size = new Size(0, 28);
-            label2.TabIndex = 0;
+            dtGrid_user.BackgroundColor = Color.WhiteSmoke;
+            dtGrid_user.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtGrid_user.Location = new Point(31, 128);
+            dtGrid_user.Name = "dtGrid_user";
+            dtGrid_user.RowHeadersWidth = 51;
+            dtGrid_user.Size = new Size(676, 390);
+            dtGrid_user.TabIndex = 23;
             // 
-            // pic_logout
+            // panel5
             // 
-            pic_logout.Image = Properties.Resources.Exit;
-            pic_logout.Location = new Point(902, 9);
-            pic_logout.Name = "pic_logout";
-            pic_logout.Size = new Size(43, 36);
-            pic_logout.SizeMode = PictureBoxSizeMode.Zoom;
-            pic_logout.TabIndex = 3;
-            pic_logout.TabStop = false;
-            pic_logout.Click += pic_logout_Click;
+            panel5.BackColor = Color.WhiteSmoke;
+            panel5.BorderStyle = BorderStyle.FixedSingle;
+            panel5.Controls.Add(tb_search_user);
+            panel5.Controls.Add(pic_search_user);
+            panel5.Location = new Point(31, 77);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(524, 38);
+            panel5.TabIndex = 22;
             // 
-            // pn_qlur
+            // tb_search_user
             // 
-            pn_qlur.BackColor = Color.FromArgb(42, 107, 167);
-            pn_qlur.Controls.Add(btn_qlur);
-            pn_qlur.Location = new Point(0, 331);
-            pn_qlur.Name = "pn_qlur";
-            pn_qlur.Size = new Size(220, 76);
-            pn_qlur.TabIndex = 3;
+            tb_search_user.BackColor = Color.WhiteSmoke;
+            tb_search_user.BorderStyle = BorderStyle.None;
+            tb_search_user.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_search_user.Location = new Point(52, 6);
+            tb_search_user.Name = "tb_search_user";
+            tb_search_user.PlaceholderText = "Nhập tên người dùng...";
+            tb_search_user.Size = new Size(467, 23);
+            tb_search_user.TabIndex = 1;
             // 
-            // pn_qlq
+            // pic_search_user
             // 
-            pn_qlq.BackColor = Color.FromArgb(42, 107, 167);
-            pn_qlq.Controls.Add(btn_qlq);
-            pn_qlq.Location = new Point(0, 408);
-            pn_qlq.Name = "pn_qlq";
-            pn_qlq.Size = new Size(220, 76);
-            pn_qlq.TabIndex = 4;
+            pic_search_user.Image = Properties.Resources.Search1;
+            pic_search_user.Location = new Point(3, 3);
+            pic_search_user.Name = "pic_search_user";
+            pic_search_user.Size = new Size(43, 30);
+            pic_search_user.SizeMode = PictureBoxSizeMode.Zoom;
+            pic_search_user.TabIndex = 0;
+            pic_search_user.TabStop = false;
+            // 
+            // btn_search_user
+            // 
+            btn_search_user.BackColor = Color.White;
+            btn_search_user.BackgroundImage = Properties.Resources.button_round2;
+            btn_search_user.FlatAppearance.BorderSize = 0;
+            btn_search_user.FlatStyle = FlatStyle.Flat;
+            btn_search_user.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_search_user.ForeColor = SystemColors.Window;
+            btn_search_user.Location = new Point(616, 76);
+            btn_search_user.Name = "btn_search_user";
+            btn_search_user.Size = new Size(91, 38);
+            btn_search_user.TabIndex = 21;
+            btn_search_user.Text = "Tìm kiếm";
+            btn_search_user.UseVisualStyleBackColor = false;
+            // 
+            // lab_dstk
+            // 
+            lab_dstk.AutoSize = true;
+            lab_dstk.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            lab_dstk.Location = new Point(232, 15);
+            lab_dstk.Name = "lab_dstk";
+            lab_dstk.Size = new Size(265, 31);
+            lab_dstk.TabIndex = 1;
+            lab_dstk.Text = "DANH SÁCH THỐNG KÊ";
+            lab_dstk.TextAlign = ContentAlignment.TopCenter;
             // 
             // dtGrid_main
             // 
@@ -120,17 +164,6 @@
             dtGrid_main.RowHeadersWidth = 51;
             dtGrid_main.Size = new Size(676, 394);
             dtGrid_main.TabIndex = 16;
-            // 
-            // panel5
-            // 
-            panel5.BackColor = Color.WhiteSmoke;
-            panel5.BorderStyle = BorderStyle.FixedSingle;
-            panel5.Controls.Add(tb_search);
-            panel5.Controls.Add(pic_search);
-            panel5.Location = new Point(34, 67);
-            panel5.Name = "panel5";
-            panel5.Size = new Size(567, 38);
-            panel5.TabIndex = 15;
             // 
             // tb_search
             // 
@@ -153,6 +186,27 @@
             pic_search.TabIndex = 0;
             pic_search.TabStop = false;
             // 
+            // pic_khtn
+            // 
+            pic_khtn.Image = Properties.Resources.logo2;
+            pic_khtn.Location = new Point(28, 28);
+            pic_khtn.Name = "pic_khtn";
+            pic_khtn.Size = new Size(170, 155);
+            pic_khtn.SizeMode = PictureBoxSizeMode.Zoom;
+            pic_khtn.TabIndex = 1;
+            pic_khtn.TabStop = false;
+            // 
+            // lab_dstknd
+            // 
+            lab_dstknd.AutoSize = true;
+            lab_dstknd.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            lab_dstknd.Location = new Point(160, 22);
+            lab_dstknd.Name = "lab_dstknd";
+            lab_dstknd.Size = new Size(427, 31);
+            lab_dstknd.TabIndex = 13;
+            lab_dstknd.Text = "DANH SÁCH TÀI KHOẢN NGƯỜI DÙNG";
+            lab_dstknd.TextAlign = ContentAlignment.TopCenter;
+            // 
             // btn_search
             // 
             btn_search.BackColor = Color.White;
@@ -167,27 +221,6 @@
             btn_search.TabIndex = 14;
             btn_search.Text = "Tìm kiếm";
             btn_search.UseVisualStyleBackColor = false;
-            // 
-            // lab_dstknd
-            // 
-            lab_dstknd.AutoSize = true;
-            lab_dstknd.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            lab_dstknd.Location = new Point(160, 22);
-            lab_dstknd.Name = "lab_dstknd";
-            lab_dstknd.Size = new Size(427, 31);
-            lab_dstknd.TabIndex = 13;
-            lab_dstknd.Text = "DANH SÁCH TÀI KHOẢN NGƯỜI DÙNG";
-            lab_dstknd.TextAlign = ContentAlignment.TopCenter;
-            // 
-            // pic_khtn
-            // 
-            pic_khtn.Image = Properties.Resources.logo2;
-            pic_khtn.Location = new Point(28, 28);
-            pic_khtn.Name = "pic_khtn";
-            pic_khtn.Size = new Size(170, 155);
-            pic_khtn.SizeMode = PictureBoxSizeMode.Zoom;
-            pic_khtn.TabIndex = 1;
-            pic_khtn.TabStop = false;
             // 
             // pn_dstk
             // 
@@ -257,6 +290,7 @@
             btn_qlur.TabIndex = 6;
             btn_qlur.Text = "Quản lý User/Role";
             btn_qlur.UseVisualStyleBackColor = false;
+            btn_qlur.Click += btn_qlur_Click;
             // 
             // pn_qlq
             // 
@@ -281,6 +315,7 @@
             btn_qlq.TabIndex = 7;
             btn_qlq.Text = "Quản lý quyền";
             btn_qlq.UseVisualStyleBackColor = false;
+            btn_qlq.Click += btn_qlq_Click;
             // 
             // Homepage
             // 
@@ -300,16 +335,20 @@
             Text = "Quản Lý Người Dùng";
             Load += Homepage_Load_1;
             pn_main.ResumeLayout(false);
+            pn_main.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pic_refresh_U).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dtGrid_user).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pic_search_user).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dtGrid_main).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic_search).EndInit();
             ((System.ComponentModel.ISupportInitialize)pic_khtn).EndInit();
             pn_dstk.ResumeLayout(false);
             pn_dstk.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pic_logout).EndInit();
             pn_qlur.ResumeLayout(false);
             pn_qlq.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)dtGrid_main).EndInit();
-            panel5.ResumeLayout(false);
-            panel5.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pic_search).EndInit();
             ResumeLayout(false);
         }
 
@@ -331,5 +370,11 @@
         private Button btn_dstk;
         private Button btn_qlur;
         private Button btn_qlq;
+        private Label lab_dstk;
+        private PictureBox pic_refresh_U;
+        private DataGridView dtGrid_user;
+        private TextBox tb_search_user;
+        private PictureBox pic_search_user;
+        private Button btn_search_user;
     }
 }
