4b67e86bcaf6ad81bf4778386eec52d7230f1f29
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-03 22:54:44 +0700

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -1,3 +1,9 @@
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+CREATE USER ADMIN IDENTIFIED BY admin123;
+GRANT ALL PRIVILEGES TO ADMIN;
+GRANT CREATE SESSION TO ADMIN;
+
 BEGIN
   EXECUTE IMMEDIATE 'DROP TABLE DANGKY';
   EXECUTE IMMEDIATE 'DROP TABLE PHANCONG';
@@ -459,9 +465,11 @@ BEGIN
     SELECT * FROM ALL_USERS;
     DBMS_SQL.return_result(curs);
 END;
-select* from ROLE_TAB_PRIVS
-select* from DBA_role_privs
-select* from user_tab_privs
-exec usp_GetAllUser
+/
+select* from ROLE_TAB_PRIVS;
+select* from DBA_role_privs;
+select* from user_tab_privs;
+exec usp_GetAllUser;
 CREATE USER C##testing IDENTIFIED BY 123456;
-select username,count(granted_role) as nOfRole from all_users, dba_role_privs where username = grantee(+) group by username
+select username,count(granted_role) as nOfRole from all_users, dba_role_privs where username = grantee(+) group by username;
+
