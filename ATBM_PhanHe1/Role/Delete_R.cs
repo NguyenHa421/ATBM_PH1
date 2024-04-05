f5159afba23eab422707daf14cb5b0430ab8c69d
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-05 18:58:54 +0700

    Update Delete_R.cs

--- ATBM_PhanHe1/Role/Delete_R.cs
+++ ATBM_PhanHe1/Role/Delete_R.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ATBM_PhanHe1.DAO;
+using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,11 +17,31 @@ namespace ATBM_PhanHe1.Role
         public Delete_R()
         {
             InitializeComponent();
+            tb_name.Text = Interface.User_Role.SelectedDeleteName;
         }
 
         private void btn_Back_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btn_Create_Click(object sender, EventArgs e)
+        {
+            string name = tb_name.Text;
+            try
+            {
+                RoleDAO.Instance.Delete_Role(name);
+                MessageBox.Show("Xóa thành công", "Thông báo");
+            }
+            catch (OracleException oe)
+            {
+                MessageBox.Show(oe.Message, "Lỗi");
+            }
+        }
+
+        private void tb_name_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
