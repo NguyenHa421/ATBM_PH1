3c3d25d3ac8a0fc3e079a2ba51f6564366cb451b
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-05 18:58:48 +0700

    Update Create_R.cs

--- ATBM_PhanHe1/Role/Create_R.cs
+++ ATBM_PhanHe1/Role/Create_R.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ATBM_PhanHe1.DAO;
+using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +22,20 @@ namespace ATBM_PhanHe1.Role
         private void btn_Back_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btn_Create_Click(object sender, EventArgs e)
+        {
+            string name = tb_role.Text;
+            try
+            {
+                RoleDAO.Instance.Add_Role(name);
+                MessageBox.Show("Tạo thành công", "Thông báo");
+            }
+            catch (OracleException oe)
+            {
+                MessageBox.Show(oe.Message, "Lỗi");
+            }
         }
     }
 }
