7310f01ed04f89212931bd0dda66850ddf0eff40
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-06 09:48:36 +0700

    Create Table_ColumnDAO.cs

---
+++ ATBM_PhanHe1/DAO/Table_ColumnDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATBM_PhanHe1.DAO
+{
+    public class Table_ColumnDAO
+    {
+        private static Table_ColumnDAO instance;
+        public static Table_ColumnDAO Instance {
+            get { if (instance == null) instance = new Table_ColumnDAO(); return Table_ColumnDAO.instance; }
+            private set { Table_ColumnDAO.instance = value; }
+        }
+        private Table_ColumnDAO() { }
+        public DataTable GetListTable()
+        {
+            return DataProvider.Instance.ExecuteQuery("SELECT * FROM user_tables WHERE table_name LIKE 'TB_%'");
+        }
+
+    }
+}
