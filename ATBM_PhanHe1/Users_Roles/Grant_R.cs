e44883c6adc82734ec460f69f40f5eb89effdaa2
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-06 12:19:36 +0700

    Update Grant_R.cs

--- ATBM_PhanHe1/Users_Roles/Grant_R.cs
+++ ATBM_PhanHe1/Users_Roles/Grant_R.cs
@@ -14,21 +14,30 @@ namespace ATBM_PhanHe1.Users_Roles
     public partial class Grant_R : Form
     {
         BindingSource tableList = new BindingSource();
-        public Grant_R()
+        BindingSource columnList = new BindingSource();
+        public Grant_R(string userName)
         {
             InitializeComponent();
+            tb_user.Text = userName;
             Load();
         }
         private void Load()
         {
             cbB_Tables.DataSource = tableList;
-            cbB_Tables.DataSource = Table_ColumnDAO.Instance.GetListTable();
+            tableList.DataSource = Table_ColumnDAO.Instance.GetListTable();
             cbB_Tables.DisplayMember = "TABLE_NAME";
         }
 
         private void btn_Back_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void cbB_Tables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbB_Column.DataSource = columnList;
+            columnList.DataSource = Table_ColumnDAO.Instance.GetListColumn(cbB_Tables.Text);
+            cbB_Column.DisplayMember = "COLUMN_NAME";
         }
     }
 }
