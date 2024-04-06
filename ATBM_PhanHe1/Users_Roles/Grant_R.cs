810406f055381f3163f6a03bac9c4055f0119899
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-07 00:17:05 +0700

    Update Grant_R.cs

--- ATBM_PhanHe1/Users_Roles/Grant_R.cs
+++ ATBM_PhanHe1/Users_Roles/Grant_R.cs
@@ -1,4 +1,5 @@
 ﻿using ATBM_PhanHe1.DAO;
+using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,6 +17,7 @@ namespace ATBM_PhanHe1.Users_Roles
     {
         BindingSource tableList = new BindingSource();
         BindingSource columnList = new BindingSource();
+        BindingSource roletableList = new BindingSource();
         public Grant_R()
         {
             InitializeComponent();
@@ -39,6 +41,43 @@ namespace ATBM_PhanHe1.Users_Roles
             cbB_Column.DataSource = columnList;
             columnList.DataSource = Table_ColumnDAO.Instance.GetListColumn(cbB_Tables.Text);
             cbB_Column.DisplayMember = "COLUMN_NAME";
+        }
+
+        private void Load_Grid()
+        {
+            dtGrid_role_table.DataSource = roletableList;
+            roletableList.DataSource = RoleDAO.Instance.ListRole_Table(tb_user.Text, cbB_Tables.Text);
+        }
+        private void bt_View_Click(object sender, EventArgs e)
+        {
+            Load_Grid();
+        }
+
+        private void bt_grant_Click(object sender, EventArgs e)
+        {
+            string role_name = tb_user.Text;
+            string table_name = cbB_Tables.Text;
+            string column_name = cbB_Column.Text;
+            int withGrantO = cB_grant.Checked ? 1 : 0;
+            List<string> privs = new List<string>();
+            foreach (var checkedItem in clb_Role.CheckedItems)
+                privs.Add(checkedItem.ToString());
+            if (privs.Count > 0)
+            {
+                try
+                {
+                    if (cB_allCol.Checked)
+                        RoleDAO.Instance.Grant_Role(role_name, privs, table_name, withGrantO);
+                    else
+                        RoleDAO.Instance.Grant_Role(role_name, privs, table_name, withGrantO, column_name);
+                    MessageBox.Show("Cấp quyền thành công", "Thông báo");
+                }
+                catch (OracleException oe)
+                {
+                    MessageBox.Show(oe.Message, "Lỗi");
+                }
+            }
+            Load_Grid();
         }
     }
 }
