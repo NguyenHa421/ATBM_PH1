f6d1446962d2e8c11482be09e8f0aa9c02a996b8
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-07 00:17:13 +0700

    Update RoleDAO.cs

--- ATBM_PhanHe1/DAO/RoleDAO.cs
+++ ATBM_PhanHe1/DAO/RoleDAO.cs
@@ -1,5 +1,7 @@
 ﻿using ATBM_PhanHe1.DTO;
 using System.Data;
+using System.Data.Common;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ATBM_PhanHe1.DAO
 {
@@ -48,5 +50,47 @@ namespace ATBM_PhanHe1.DAO
             int result = DataProvider.Instance.ExecuteNonQuery(query);
             return result > 0;
         }
+        
+        public DataTable ListRole_Table(string roleName, string tableName)
+        {
+            return DataProvider.Instance.ExecuteQuery($"select d.role, p.table_name, p.column_name, p.privilege from dba_roles d left join role_tab_privs p on p.role = d.role where lower(d.role) like lower('%{roleName}%') and lower(p.table_name) like lower('%{tableName}%')");
+        }
+        
+        public bool Grant_Role(string role_name, List<string> privs, string table_name, int withGrantOption)
+        {
+            string privStr = "";
+            foreach (string str in privs)
+                privStr += str + ",";
+            privStr = privStr.Remove(privStr.Length - 1);
+            string query = $"begin grant_privilege_to_role('{role_name}','{privStr}','{table_name}','{withGrantOption}');end;";
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+        public bool Grant_Role(string role_name, List<string> privs, string table_name, int withGrantOption, string column_name)
+        {
+            string privStr = "";
+            foreach (string str in privs)
+            {
+                privStr += str;
+                if (str == "Update")
+                    privStr += "(" + column_name + ")";
+                privStr += ",";
+            }
+            privStr = privStr.Remove(privStr.Length - 1);
+            string query = $"begin grant_privilege_to_role('{role_name}','{privStr}','{table_name}','{withGrantOption}');end;";
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+        public bool Revoke_Role(string role_name, List<string> privs, string table_name)
+        {
+            string privStr = "";
+            foreach (string str in privs)
+                privStr += str + ",";
+            privStr = privStr.Remove(privStr.Length - 1);
+            string query = $"begin revoke_privilege_to_role('{role_name}','{privStr}','{table_name}');end;";
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+      
     }
 }
