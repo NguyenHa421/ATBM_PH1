35f8c2dc4003f6526fcb984637883726ef59996b
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-06 09:48:46 +0700

    Update Grant_U.cs

--- ATBM_PhanHe1/Users_Roles/Grant_U.cs
+++ ATBM_PhanHe1/Users_Roles/Grant_U.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ATBM_PhanHe1.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,26 @@ namespace ATBM_PhanHe1.Users_Roles
 {
     public partial class Grant_U : Form
     {
+        BindingSource tableList = new BindingSource();
         public Grant_U()
         {
             InitializeComponent();
+            Load();
         }
-
+        private void Load()
+        {
+            cbB_Tables.DataSource = tableList;
+            cbB_Tables.DataSource = Table_ColumnDAO.Instance.GetListTable();
+            cbB_Tables.DisplayMember = "TABLE_NAME";
+        }
         private void btn_Back_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void cbB_Tables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
