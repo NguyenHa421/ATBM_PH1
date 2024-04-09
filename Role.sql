3e9367e48cd59c5f2fea3a1d904a990ae7636f6e
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-04-09 14:00:46 +0700

    Update Role.sql

--- Role.sql
+++ Role.sql
@@ -32,29 +32,10 @@ BEGIN
     EXECUTE IMMEDIATE ('GRANT '||privilege_name|| ' ON '||table_name||' TO '||role_name);
 end;
 /
-begin
-    grant_privilege_to_role ('C##TEST42', 'INSERT', 'TB_SINHVIEN');
-end;
-select d.role, p.table_name, p.column_name, p.privilege from dba_roles d left join role_tab_privs p on p.role = d.role where d.role = 'C##TEST42';
 --THU HOI QUYEN CUA ROLE
 CREATE OR REPLACE PROCEDURE revoke_privilege_to_role (role_name IN VARCHAR2, privilege_name IN VARCHAR2, table_name IN VARCHAR2)
 AS
 BEGIN
-    EXECUTE IMMEDIATE ('REVOKE '||privilege_name|| 'ON '||table_name||' FROM '||role_name);
+    EXECUTE IMMEDIATE ('REVOKE '||privilege_name|| ' ON '||table_name||' FROM '||role_name);
 END;
-
 /
-
---XEM QUYEN ROLE
-CREATE OR REPLACE PROCEDURE view_priv_role (role_name IN VARCHAR2, cur OUT sys_refcursor)
-AS
-    tmp_count INT;
-BEGIN
-    SELECT COUNT(*) INTO tmp_count FROM session_roles;
-    IF(tmp_count != 0) THEN
-        OPEN cur FOR SELECT * FROM role_tab_privs WHERE ROLE = role_name; 
-    ELSE 
-        DBMS_OUTPUT.PUT_LINE('Role khong ton tai');
-    END IF;
-END;
-
