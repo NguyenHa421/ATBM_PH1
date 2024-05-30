412148a649df911c29488e50de417eb233dd9c2b
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-05-31 01:57:50 +0700

    Update Program.cs

--- ATBM_PhanHe1/Program.cs
+++ ATBM_PhanHe1/Program.cs
@@ -13,7 +13,7 @@ namespace ATBM_PhanHe1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Home_Login.Login());
+            Application.Run(new PhanHe2.MainBase());
         }
     }
 }
