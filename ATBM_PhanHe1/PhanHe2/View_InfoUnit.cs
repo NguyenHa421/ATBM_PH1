0201d91c0a76305ef84b45cca30a2d93a202a797
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-05-31 01:58:02 +0700

    Create View_InfoUnit.cs

---
+++ ATBM_PhanHe1/PhanHe2/View_InfoUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.PhanHe2
+{
+    public partial class View_InfoUnit : Form
+    {
+        public View_InfoUnit()
+        {
+            InitializeComponent();
+        }
+
+        private void lb_name_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
