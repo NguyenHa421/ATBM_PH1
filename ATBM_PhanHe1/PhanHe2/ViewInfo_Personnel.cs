928f31b7739f239890f5fce7d2adbec02dbebe34
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-05-31 01:58:08 +0700

    Create ViewInfo_Personnel.cs

---
+++ ATBM_PhanHe1/PhanHe2/ViewInfo_Personnel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.PhanHe2
+{
+    public partial class ViewInfo_Personnel : Form
+    {
+        public ViewInfo_Personnel()
+        {
+            InitializeComponent();
+        }
+
+        private void lb_name_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
