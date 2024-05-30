60cc2de9a7a2917e67aab9d31122978e03b74e8a
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-05-31 01:57:24 +0700

    Create Update_Info_Personnel.cs

---
+++ ATBM_PhanHe1/PhanHe2/Update_Info_Personnel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.PhanHe2
+{
+    public partial class Update_Info_Personnel : Form
+    {
+        public Update_Info_Personnel()
+        {
+            InitializeComponent();
+        }
+
+        private void lb_Info_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            Confirm_Update confirm_Update = new Confirm_Update();
+            confirm_Update.ShowDialog();
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
