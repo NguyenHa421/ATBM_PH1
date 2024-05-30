051f605953c43e3c7a3b0a309e45a66db29279b8
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-05-31 01:56:32 +0700

    Create Add_Unit.cs

---
+++ ATBM_PhanHe1/PhanHe2/Add_Unit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.PhanHe2
+{
+    public partial class Add_Unit : Form
+    {
+        public Add_Unit()
+        {
+            InitializeComponent();
+        }
+
+        private void cbB_program_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
