054428aaebeed6c857bceb5513796db3894a0ddb
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-05-31 01:57:30 +0700

    Create Update_Student.cs

---
+++ ATBM_PhanHe1/PhanHe2/Update_Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.PhanHe2
+{
+    public partial class Update_Student : Form
+    {
+        public Update_Student()
+        {
+            InitializeComponent();
+        }
+
+        private void lb_phone_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
