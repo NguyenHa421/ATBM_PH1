b8363d2b9350f9028dee1e7f3889f2a5b25e8347
KamisatoNoel <105796003+KamisatoNoel@users.noreply.github.com>
2024-05-31 01:57:54 +0700

    Create View_InfoStudent.cs

---
+++ ATBM_PhanHe1/PhanHe2/View_InfoStudent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATBM_PhanHe1.PhanHe2
+{
+    public partial class View_InfoStudent : Form
+    {
+        public View_InfoStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_id_unit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
