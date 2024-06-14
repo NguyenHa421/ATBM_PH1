5cd8aac8666006f376d0605f7dddf05c4a7b8a66
Baodi0 <baodi18020@gmail.com>
2024-06-14 22:03:12 +0700

    Update Audit.sql

--- Audit.sql
+++ Audit.sql
@@ -15,3 +15,67 @@ CONN NV107/NV107
 SELECT * FROM ADMIN.TB_HOCPHAN;
 --xem nhat ky audit
 SELECT * FROM DBA_AUDIT_TRAIL WHERE obj_name = 'TB_HOCPHAN';
+
+/
+BEGIN
+     DBMS_FGA.DROP_POLICY(
+     OBJECT_SCHEMA =>'ADMIN',
+     OBJECT_NAME =>'TB_DANGKY',
+     POLICY_NAME =>'FGA_CHECK_SCORE_UPDATE');
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+--su dung fine-grained audit ghi nhat ky update cua moi user tren bang tb_dangky
+BEGIN
+  DBMS_FGA.ADD_POLICY(
+    object_schema   => 'ADMIN',
+    object_name     => 'TB_DANGKY',
+    policy_name     => 'FGA_CHECK_SCORE_UPDATE',
+    audit_condition => 'SYS_CONTEXT(''USERENV'', ''SESSION_USER'') NOT IN (SELECT MANV FROM ADMIN.TB_NHANSU WHERE VAITRO = ''Giang vien'')',
+    statement_types => 'UPDATE',
+    audit_column    => 'DIEMTH, DIEMQT, DIEMCK, DIEMTK'
+  );
+END;
+/
+conn NV052/NV052;
+BEGIN
+  UPDATE ADMIN.TB_DANGKY
+  SET DIEMTH = 9, DIEMQT = 9, DIEMCK = 9, DIEMTK = 9
+  WHERE MASV = 'SV127123' AND MAHP = 'HP21' AND NAMHOC = 2024 AND HOCKY = 3 AND LOP = 'CLC';
+END;
+
+/
+BEGIN
+     DBMS_FGA.DROP_POLICY(
+     OBJECT_SCHEMA =>'ADMIN',
+     OBJECT_NAME =>'TB_NHANSU',
+     POLICY_NAME =>'FGA_CHECK_PHUCAP_SELECT');
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+--su dung fine-grained audit ghi nhat ky ??c phu cap nguoi khac tren bang TB_NHANSU
+BEGIN
+  DBMS_FGA.ADD_POLICY(
+    object_schema   => 'ADMIN',
+    object_name     => 'TB_NHANSU',
+    policy_name     => 'FGA_CHECK_PHUCAP_SELECT',
+    audit_condition => 'MANV != SYS_CONTEXT(''USERENV'', ''SESSION_USER'')',
+    audit_column    => 'PHUCAP',
+    statement_types => 'SELECT',
+    enable =>TRUE
+);
+END;
+/
+
+conn NV107/NV107;
+SELECT * FROM ADMIN.TB_NHANSU WHERE MANV = 'NV053';
+/
+
+SELECT * FROM DBA_FGA_AUDIT_TRAIL;
+SELECT * FROM DBA_AUDIT_TRAIL;
+
+select * from ALL_AUDIT_POLICIES
