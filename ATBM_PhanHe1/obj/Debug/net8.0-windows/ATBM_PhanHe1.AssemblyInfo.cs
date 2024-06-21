1e52b0b15d583b3f2614e5b78953455d44e05e98
SvvPhenex <phucdat112003@gmail.com>
2024-06-21 22:40:57 +0700

    sửa cái audit

--- ATBM_PhanHe1/obj/Debug/net8.0-windows/ATBM_PhanHe1.AssemblyInfo.cs
+++ ATBM_PhanHe1/obj/Debug/net8.0-windows/ATBM_PhanHe1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATBM_PhanHe1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5a713f199e18ca59683db4fd77c9415621b1be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598de2db4b8fb2e0cb21df38c6a87fab632364d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATBM_PhanHe1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATBM_PhanHe1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
